e4f42b49274dde6a1c84e4f9438eb3503591f9ba
raindancin <206476473+raindancin@users.noreply.github.com>
2026-07-26 20:36:27 -0700

    Update lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -102,21 +102,4 @@ return {
 			})
 		end,
 	},
-	-- Mason: Only load when needed
-	{
-		"williamboman/mason.nvim",
-		cmd = "Mason",
-		keys = { { "<leader>m", "<cmd>Mason<cr>", desc = "Mason" } },
-		build = ":MasonUpdate",
-		opts = {
-			ui = {
-				border = "rounded",
-				icons = {
-					package_installed = "󰗠",
-					package_pending = "󱍷",
-					package_uninstalled = "󰚃",
-				},
-			},
-		},
-	},
 }
