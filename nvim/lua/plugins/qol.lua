42c19fe4d91eba691e74e87e76794a825772a4fd
raindancin <206476473+raindancin@users.noreply.github.com>
2026-07-26 20:36:00 -0700

    Update and rename movement.lua to qol.lua

--- nvim/lua/plugins/qol.lua
+++ nvim/lua/plugins/qol.lua
@@ -66,4 +66,20 @@ return {
 			},
 		},
 	},
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		keys = { { "<leader>m", "<cmd>Mason<cr>", desc = "Mason" } },
+		build = ":MasonUpdate",
+		opts = {
+			ui = {
+				border = "rounded",
+				icons = {
+					package_installed = "󰗠",
+					package_pending = "󱍷",
+					package_uninstalled = "󰚃",
+				},
+			},
+		},
+	},
 }
