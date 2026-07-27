1678e04f3e952500b3cbff9da1cb6d636e055153
raindancin <206476473+raindancin@users.noreply.github.com>
2026-07-26 20:29:35 -0700

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,5 +6,41 @@ return {
 		config = function()
 			vim.cmd.colorscheme("jetbrains_dark")
 		end,
+	},
+	{
+		"raindancin/solarized",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("solarized").setup({
+  			transparent = false,      -- set true to use your terminal's own background
+  			terminal_colors = true,   -- sync :terminal ANSI colors to this palette
+  			style = "auto",           -- "auto" | "dark" | "light" -- see Dark / light below
+
+  			styles = {
+    			comments = { italic = true },
+    			keywords = { italic = false },
+    			functions = {},
+    			variables = {},
+  			},
+
+  			integrations = {
+    			treesitter = true,
+    			lsp = true,
+    			telescope = true,
+    			cmp = true,
+    			lualine = true,
+    			leap = true,
+    			mason = true,
+    			gitsigns = true,
+    			which_key = true,
+    			indent_blankline = false,
+    			render_markdown = true,
+    			oil = false,
+    			lazy = true,
+  		},
+		})
+			vim.cmd.colorscheme("solarized")
+		end
 	}
 }
