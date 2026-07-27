bdb8d5193d187cd8e34339a44f138dab99b32e87
raindancin <206476473+raindancin@users.noreply.github.com>
2026-07-26 20:29:47 -0700

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,10 +2,6 @@ return {
 	{
 		"raindancin/jetbrains_dark",
 		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("jetbrains_dark")
-		end,
 	},
 	{
 		"raindancin/solarized",
